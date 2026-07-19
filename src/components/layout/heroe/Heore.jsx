4a03c99038783e9fc6a52fc3370150ae58238384
lunaGatica <89168675+lunaGatica@users.noreply.github.com>
2026-07-19 18:41:59 -0300

    Add files via upload

---
+++ src/components/layout/heroe/Heore.jsx
@@ -0,0 +1,14 @@
+import styles from './Heroe.module.css';
+
+function Heroe () {
+
+    return (
+
+        <section className={styles.heroe}>
+
+
+        </section>
+    );
+}
+
+export default Heroe;
